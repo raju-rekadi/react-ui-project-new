64457dec745e33bba906b8f9860582af1b44f6e9
raju <rajurekadi7@gmail.com>
2023-06-22 22:30:38 +0530

    first commit

--- src/audit/Audit.jsx
+++ src/audit/Audit.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { TableComponent } from "./TableComponent";
 import BootstrapTable from "react-bootstrap-table-next";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
